151ab49bc3f39066a4b8cef20533dca616c78e17
GokuLoro <sahatqiul@gmail.com>
2026-05-29 18:42:41 +0200

    “desuisse-logo-white.png”

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -374,7 +374,7 @@ export default function HomePage() {
             <img
               src="/images/desuisse-logo-white.png"
               alt="DeSuisse Luxury Jewellery"
-              style={{ width: 'auto', height: '100', display: 'block' }}
+              style={{ width: 'auto', height: '50', display: 'block' }}
             />
           </div>
           <Link href="/shop" className="btn-gold fade-up fade-up-delay-2" style={{ display: 'inline-block', fontSize: 12, letterSpacing: '0.25em' }}>
